b6302711181f69ed11db3b1168a39087499c7909
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-10 16:16:56 +0430

    feat: create site TopBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { useState } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -16,8 +12,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
+        <html lang="en" dir="rtl">
+            <body className="font-iransans-regular">
+                {children}
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import TopBar from "@/components/modules/topbar/TopBar";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            
+        <main className="">
+            <TopBar />
         </main>
     );
 }
