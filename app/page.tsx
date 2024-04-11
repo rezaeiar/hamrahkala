8c399a0d613c05c45aa49db6393e363c74d0b624
[rezaeiar] <rezaeiar0@gmail.com>
2024-04-11 19:42:15 +0430

    feat: create ProductsSliderHeader in Index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Categories from "@/components/templates/index/categories/Categories";
 import HeaderSlider from "@/components/templates/index/header-slider/HeaderSlider";
+import ProductSliderHeader from "@/components/modules/product-slider-header/ProductSliderHeader";
 
 export default function Home() {
     return (
-        <main className="flex flex-col gap-4">
+        <main className="flex flex-col gap-6">
             <HeaderSlider />
             <Categories />
+            <ProductSliderHeader />
         </main>
     );
 }
---
+++ components/modules/product-slider-header/ProductSliderHeader.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProductSliderHeader() {
+    return (
+        <div className=''>
+            <div className="container">
+                <div className="bg-general-30 rounded flex items-center justify-between h-14 text-xs text-general-100 px-4">
+                    <h3 className='text-sm font-iransans-bold'>
+                        جدیدترین گوشی ها
+                    </h3>
+                    <Link href={'/'} className='flex gap-1 items-center text-general-80'>
+                        مشاهده همه
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-3 h-3 text-general-80">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
